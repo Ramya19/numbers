b6f52dece8e6f56cf60b9526993d8eb1880b5add
Ramya19 <ramyamohanit@gmail.com>
2016-07-15 18:24:03 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,9 @@
+ int n=0;
+        // TODO code application logic here
+        if(n%2==0)
+        {
+            System.out.println("it is  even");
+        }
+        else
+            System.out.println("it is odd");
+    }
