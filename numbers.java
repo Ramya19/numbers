76f9def88ff08d59f28a8dca3eba12e673f862e6
Ramya19 <ramyamohanit@gmail.com>
2016-07-15 18:16:49 +0530

    Add files via upload

---
+++ numbers.java
@@ -0,0 +1,10 @@
+if(n>0)
+        {
+            System.out.println("it is positive");
+        }
+        else if(n<0)
+                {
+                    System.out.println("it is negative");
+                }
+        else
+        System.out.println("zero");
